60501dbd539c55cab530abc8586072c615957f5c
nicholasnlawson <144561408+nicholasnlawson@users.noreply.github.com>
2025-03-15 20:58:11 +0000

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -710,6 +710,6 @@ def get_medication_details():
 
 
 if __name__ == '__main__':
-    # Get port from environment variable or default to 5001
+    import os
     port = int(os.environ.get('PORT', 5001))
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=port)
